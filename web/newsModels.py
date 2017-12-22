d447b2aad699af1993b69868bf94c21304ffca03
ahmad <ahmadubuntu@gmail.com>
2017-12-22 12:41:54 +0330

    correction of __str__ in newsModel

--- web/newsModels.py
+++ web/newsModels.py
@@ -9,5 +9,6 @@ class News(models.Model):
     title = models.CharField(max_length=250)
     text = models.TextField()
     date = models.DateTimeField()
-    def __unicode__(self):
+    #def __unicode__(self):
+    def __str__(self):
         return self.title
