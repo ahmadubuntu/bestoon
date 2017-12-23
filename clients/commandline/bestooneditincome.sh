b5da9d01ce66cced746f582f0dfc595d1fb14f90
ahmad <ahmadubuntu@gmail.com>
2017-12-23 12:08:57 +0330

    edit income and expense added to backend

---
+++ clients/commandline/bestooneditincome.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source "$(dirname $0)"/bestoonconfig.sh
+
+print_usage()
+{
+    echo "Use this script to submit expense reports to ${BASE_URL}"
+    echo "Usage: ${0} <Amount> <Description>. Eg:"
+    echo "Usage: ${0} 1000 Donation"
+}
+
+AMOUNT=$1
+shift
+ID=$1
+shift
+
+TEXT=$*
+if [ -z "$TEXT" ]; then
+    print_usage
+    exit 1
+fi
+
+curl --data "token=$TOKEN&amount=$AMOUNT&text=$TEXT&id=$ID" $BASE_URL/edit/income/
